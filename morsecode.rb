461c1913e173eca28039d6ee1c79eb555f365a93
Caren-Koroeny <carensiya5@gmail.com>
2023-05-10 09:50:09 +0300

    Fix linters errors

--- morsecode.rb
+++ morsecode.rb
@@ -1,21 +1,22 @@
 def decode_char(char)
-    morsecode = {
-        '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', '.' => 'E',
-        '..-.' => 'F', '--.' => 'G', '....' => 'H', '..' => 'I', '.---' => 'J',
-        '-.-' => 'K', '.-..' => 'L', '--' => 'M', '-.' => 'N', '---' => 'O',
-        '.---.' => 'P', '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T',
-        '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X', '-.--' => 'Y',
-        '--..' => 'Z'
-      }
-      puts morsecode[char]
-    end
+  morsecode = {
+    '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', '.' => 'E',
+    '..-.' => 'F', '--.' => 'G', '....' => 'H', '..' => 'I', '.---' => 'J',
+    '-.-' => 'K', '.-..' => 'L', '--' => 'M', '-.' => 'N', '---' => 'O',
+    '.---.' => 'P', '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T',
+    '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X', '-.--' => 'Y',
+    '--..' => 'Z'
+  }
+  puts morsecode[char]
+end
 
-    def decode_word(word)
-        puts word.split.map {  |char| decode_char(char) }.join
-    end
-    def decode_fullmessage(message)
-        puts message.split('  ').map { |char| decode_word(char) }.join(' ')
-      end
-      decode_char('.-')
-      decode_word('-- -.--')
-      decode_fullmessage('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
+def decode_word(word)
+  puts word.split.map { |char| decode_char(char) }.join
+end
+
+def decode_fullmessage(message)
+  puts message.split('  ').map { |char| decode_word(char) }.join(' ')
+end
+decode_char('.-')
+decode_word('-- -.--')
+decode_fullmessage('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
