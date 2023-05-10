6eb964cc71d9463c2a71e9f07ba36f5977137dc3
Caren-Koroeny <carensiya5@gmail.com>
2023-05-10 09:43:28 +0300

    Decode full message

--- morsecode.rb
+++ morsecode.rb
@@ -1,4 +1,4 @@
-def decode_dict(char)
+def decode_char(char)
     morsecode = {
         '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', '.' => 'E',
         '..-.' => 'F', '--.' => 'G', '....' => 'H', '..' => 'I', '.---' => 'J',
@@ -9,3 +9,10 @@ def decode_dict(char)
       }
       puts morsecode[char]
     end
+
+    def decode_word(word)
+        puts word.split.map {  |char| decode_char(char) }.join
+    end
+    def decode_fullmessage(message)
+        puts message.split('  ').map { |char| decode_word(char) }.join(' ')
+      end
